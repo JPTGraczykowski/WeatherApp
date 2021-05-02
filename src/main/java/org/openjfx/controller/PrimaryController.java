eb4ca102a180eab33a2e2c5c3a01e0437ed47728
jedrzejgraczykowski <jedrzej.graczykowski@htdevelopers.com>
2021-05-02 13:54:23 +0200

    Fix the structure and add first window with controller

---
+++ src/main/java/org/openjfx/controller/PrimaryController.java
@@ -0,0 +1,43 @@
+package org.openjfx.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class PrimaryController {
+
+  @FXML
+  private TextField firstCountry;
+
+  @FXML
+  private TextField secondCountry;
+
+  @FXML
+  private TextField firstCity;
+
+  @FXML
+  private TextField secondCity;
+
+  @FXML
+  private Label helpLabel1;
+
+  @FXML
+  private Label helpLabel2;
+
+  @FXML
+  private Label titleLabel;
+
+  @FXML
+  private Label errorLabel;
+
+  @FXML
+  void firstShowButtonAction() {
+    System.out.println("First Button Clicked!");
+  }
+
+  @FXML
+  void secondShowButtonAction() {
+    System.out.println("Second Button Clicked!");
+  }
+
+}
